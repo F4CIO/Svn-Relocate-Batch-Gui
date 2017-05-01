2ce1fcff9ae1ea9bc12c4ca20b5b1985ee2928f3
F4CIO <f4cio@f4cio.com>
2017-05-01 23:06:39 +0200

    Initial version from SourceCode folder

---
+++ CraftSynth.BuildingBlocks.Generated/CraftSynth.BuildingBlocks.UI.Web.UriHandler.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CraftSynth.BuildingBlocks.UI.Web
+{
+	public class UriHandler
+	{
+		//Pattern library and tools:
+		// http://regexlib.com/Default.aspx
+
+		//        foo://example.com:8042/over/there?name=ferret#nose
+		//              \__host___/\port|    
+		//        \_/   \______________/\_________/\__________/ \__/
+		//         |           |             |           |        |
+		//      scheme     authority       path        query   fragment
+		//        s0          a0            p0          q0        f0
+		public const string Pattern_Uri = @"^(?<s1>(?<s0>[^:/\?#]+):)?(?<a1>"
+												+ @"//(?<a0>[^/\?#]*))?(?<p0>[^\?#]*)"
+												+ @"(?<q1>\?(?<q0>[^#]*))?"
+												+ @"(?<f1>#(?<f0>.*))?";
+
+		/// <summary>
+		/// Checks wether passed string is valid full url.
+		/// </summary>
+		/// <param name="strToCheck">Uri to check where protocol part must be included. Examples:
+		/// '<example>http://www.mywebsite.com:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1&mykey2=myvalue2#myanchor</example>'
+		/// '<example>http://www.mywebsite.com</example>'
+		/// </param>
+		/// <returns></returns>
+		public static bool IsUri(String strToCheck)
+		{
+			Regex objAplhaPattern = new Regex(Pattern_Uri);
+			return objAplhaPattern.IsMatch(strToCheck);
+		}
+
+		/// <summary>
+		/// Gets protocol part. Example: http, https
+		/// </summary>
+		/// <param name="uri">Uri from which part should be extracted where protocol part must be included. Examples:
+		/// '<example>http://www.mywebsite.com:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1&mykey2=myvalue2#myanchor</example>'
+		/// '<example>http://www.mywebsite.com</example>'
+		/// </param>
+		/// <returns>Requested part. Example: http, https.<value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetProtocol(string uri)
+		{
+			string protocol = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				protocol = match.Groups["s0"].Value;
+			}
+			return protocol;
+		}
+
+		/// <summary>
+		/// Gets authority part. Example: www.mywebsite.com:8080
+		/// </summary>
+		/// <param name="uri">Uri from which part should be extracted where protocol part must be included. Examples:
+		/// 'http://www.mywebsite.com:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor'
+		/// 'https://www.mywebsite.com'
+		/// </param>
+		/// <returns>Requested part. Example: www.mywebsite.com:8080. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetAuthority(string uri)
+		{
+			string authority = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				authority = match.Groups["a0"].Value;
+			}
+			return authority;
+		}
+
+		/// <summary>
+		/// Gets host part. Examples: '192.168.0.1', 'www.mywebsite.com'
+		/// </summary>
+		/// <param name="uri">
+		/// Uri from which part should be extracted where protocol part must be included. Examples: 
+		/// 'http://192.168.0.1:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor',
+		/// 'https://www.mywebsite.com'.
+		/// </param>
+		/// <returns>Requested part. Examples: '192.168.0.1', 'www.mywebsite.com'. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetHost(string uri)
+		{
+			string host = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				string authority = match.Groups["a0"].Value;
+				if (!string.IsNullOrEmpty(authority))
+				{
+					string[] parts = authority.Split(':');
+					if (parts.Length > 0)
+					{
+						host = parts[0];
+					}
+				}
+			}
+			return host;
+		}
+
+		/// <summary>
+		/// Gets port part. Example: '8080'
+		/// </summary>
+		/// <param name="uri">
+		/// Uri from which part should be extracted where protocol part must be included. Examples: 
+		/// 'http://192.168.0.1:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor',
+		/// 'https://www.mywebsite.com'.
+		/// </param>
+		/// <returns>Requested part. Example: '8080'. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetPort(string uri)
+		{
+			string port = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				string authority = match.Groups["a0"].Value;
+				if (!string.IsNullOrEmpty(authority))
+				{
+					string[] parts = authority.Split(':');
+					if (parts.Length > 1)
+					{
+						port = parts[1];
+					}
+				}
+			}
+			return port;
+		}
+
+		/// <summary>
+		/// Gets path part. Example: 'mywebapp/mydir/mypage.htm'
+		/// </summary>
+		/// <param name="uri">
+		/// Uri from which path should be extracted where protocol part must be included. Examples: 
+		/// 'http://192.168.0.1:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor',
+		/// 'https://www.mywebsite.com'.
+		/// </param>
+		/// <returns>Requested part. Example: 'mywebapp/mydir/mypage.htm'. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetPath(string uri)
+		{
+			string path = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				path = match.Groups["p0"].Value;
+			}
+			return path;
+		}
+
+		/// <summary>
+		/// Gets key/value pairs list.
+		/// </summary>
+		/// <param name="uri">
+		/// Uri from which pairs should be extracted where protocol part must be included. Examples: 
+		/// 'http://192.168.0.1:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor',
+		/// 'https://www.mywebsite.com'.
+		/// </param>
+		/// <returns>Requested part. Example: '{(mykey1,myvalue1),(mykey2,myvalue2)}'. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static Dictionary<string, string> GetKeyValuePairs(string uri)
+		{
+			Dictionary<string, string> pairs = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				string query = match.Groups["q0"].Value;
+				string[] keyValuePairsArray = query.Split('&');
+
+				pairs = new Dictionary<string, string>();
+				foreach (string keyValuePairString in keyValuePairsArray)
+				{
+					//This should be tested before use:
+					//Regex.Match(keyValuePairString, @"^(?<key>[^?=]+)=(?<value>[^=#]*)&").Groups["key"].Value
+					//Regex.Match(keyValuePairString, @"^(?<key>[^?=]+)=(?<value>[^=#]*)&").Groups["value"].Value
+					pairs.Add(keyValuePairString.Split('=')[0], keyValuePairString.Split('=')[1]);
+				}
+
+			}
+			return pairs;
+		}
+
+		/// <summary>
+		/// Gets fragment part. Example: 'myanchor'
+		/// </summary>
+		/// <param name="uri">
+		/// Uri from which fragment should be extracted where protocol part must be included. Examples: 
+		/// 'http://192.168.0.1:8080/mywebapp/mydir/mypage.htm?mykey1=myvalue1(and sign)mykey2=myvalue2#myanchor',
+		/// 'https://www.mywebsite.com'.
+		/// </param>
+		/// <returns>Requested part. Example: 'myanchor'. <value>null</value> if uri is invalid or part was not found.</returns>
+		public static string GetFragment(string uri)
+		{
+			string fragment = null;
+			Regex regex = new Regex(Pattern_Uri);
+			Match match = regex.Match(uri);
+			if (match.Success)
+			{
+				fragment = match.Groups["f0"].Value;
+			}
+			return fragment;
+		}
+
+		/// <summary>
+		/// Gets for example http://www.google.com:8080/MyApplication
+		/// </summary>
+		/// <returns></returns>
+		public static string GetAbsoluteWebsiteRoot()
+		{
+			if (HttpContext.Current.Request.ApplicationPath.Length <= 1)
+			{//not path so value is '/'
+				return HttpContext.Current.Request.Url.AbsoluteUri.Replace(
+					HttpContext.Current.Request.Url.PathAndQuery,
+					string.Empty);
+			}
+			else
+			{//path exists
+				return HttpContext.Current.Request.Url.AbsoluteUri.Replace(
+					HttpContext.Current.Request.Url.PathAndQuery.Replace(HttpContext.Current.Request.ApplicationPath, string.Empty),
+					string.Empty);
+			}
+		}
+
+
+	}
+
+	//Other regular expression pattern examples:
+	//     Match only alphanumeric characters along with the characters -, +, ., and any whitespace, with the stipulation that there is at least one of these characters and no more than 10 of these characters:
+
+	//    ^([\w\.+-]|\s){1,10}$
+
+	// Match a date in the form ##/##/#### where the day and month can be a one- or two-digit value, and year can either be a two- or four-digit value:
+
+	//    ^\d{1,2}\/\d{1,2}\/\d{2,4}$
+
+	// Match a time to be entered with an optional am or pm extension (note that this regular expression also handles military time):
+
+	//    ^\d{1,2}:\d{2}\s?([ap]m)?$
+
+	// Match an IP address:
+
+	//    ^([0-2]?[0-5]?[0-5]\.){3}[0-2]?[0-5]?[0-5]$
+
+	// Verify that an email address is in the form name@address where address is not an IP address:
+
+	//    ^[A-Za-z0-9_\-\.]+@(([A-Za-z0-9\-])+\.)+([A-Za-z\-])+$
+
+	// Verify that an email address is in the form name@address where address is an IP address:
+
+	//    ^[A-Za-z0-9_\-\.]+@([0-2]?[0-5]?[0-5]\.){3}[0-2]?[0-5]?[0-5]$
+
+	// Match only a dollar amount with the optional $ and + or - preceding characters (note that any number of decimal places may be added):
+
+	//    ^\$?[+-]?[\d,]*(\.\d*)?$
+
+	//This is similar to the previous regular expression except that only up to two decimal places are allowed:
+
+	//    ^\$?[+-]?[\d,]*\.?\d{0,2}$
+
+	// Match a credit card number to be entered as four sets of four digits separated with a space, -, or no character at all:
+
+	//    ^((\d{4}[- ]?){3}\d{4})$
+
+	// Match a zip code to be entered either as five digits with an optional four-digit extension:
+
+	//    ^\d{5}(-\d{4})?$
+
+	// Match a North American phone number with an optional area code and an optional - character to be used in the phone number and no extension:
+
+	//    ^(\(?[0-9]{3}\)?)?\-?[0-9]{3}\-?[0-9]{4}$
+
+	// Match a phone number similar to the previous regular expression, but allow an optional five-digit extension prefixed with either ext or extension:
+
+	//    ^(\(?[0-9]{3}\)?)?\-?[0-9]{3}\-?[0-9]{4}(\s*ext(ension)?[0-9]{5})?$
+
+	// Match a full path beginning with the drive letter and optionally match a filename with a three-character extension (note that no .. characters signifying to move up the directory hierarchy are allowed, nor is a directory name with a . followed by an extension):
+
+	//    ^[a-zA-Z]:[\\/]([_a-zA-Z0-9]+[\\/]?)*([_a-zA-Z0-9]+\.[_a-zA-Z0-9]{0,3})?$
+}
---
+++ CraftSynth.SvnRelocateBatchGui/FormMain.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CraftSynth.BuildingBlocks.Common;
+using CraftSynth.BuildingBlocks.Logging;
+
+namespace CraftSynth.SvnRelocateBatchGui
+{
+	
+	public partial class FormMain : Form
+	{
+		private List<Item> _allItems = new List<Item>();
+		private List<string> _allSvnRootUrls = new List<string>();
+
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			if (this.folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
+			{
+				this.btnBrowse.Text = this.folderBrowserDialog.SelectedPath;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.folderBrowserDialog.SelectedPath = "d:\\Projects";
+			if (!Directory.Exists(this.folderBrowserDialog.SelectedPath))
+			{
+				MessageBox.Show("You must select existing folder root folder of all projects (local copies) in step 1.");
+			}
+			else
+			{
+				var allFolders = BuildingBlocks.IO.FileSystem.GetFolderPaths(this.folderBrowserDialog.SelectedPath, true);
+				var allSvnFolders = allFolders.Where(f=>f.ToLower().EndsWith("\\.svn")).ToList();
+				foreach (var svnFolder in allSvnFolders)
+				{
+					var item = new Item(this.tbSvnExePath.Text, svnFolder, this.tbNewSvnRootUrl.Text.ToNonNullString());
+					this.tbAllProjects.AppendText(item.folder);
+					this.tbAllProjects.AppendText(" | ");
+					this.tbAllProjects.AppendText(item.svnUrl);
+					this.tbAllProjects.AppendText(" ==> ");
+					//this.tbAllProjects.AppendText(item.svnRootUrl);
+					//this.tbAllProjects.AppendText(" | "); 
+					this.tbAllProjects.AppendText(item.svnNewUrl);
+					this.tbAllProjects.AppendText("\r\n");
+					this._allItems.Add(item);
+
+					if (!this._allSvnRootUrls.Contains(item.svnRootUrl))
+					{
+						this._allSvnRootUrls.Add(item.svnRootUrl);
+						this.tbSvnRootUrlsFound.AppendText(item.svnRootUrl+"\r\n");
+					}
+				}
+			}
+		}
+
+		private void btnReloacteAll_Click(object sender, EventArgs e)
+		{
+			this.tbOutput.Clear();
+			if (!File.Exists(this.tbSvnExePath.Text))
+			{
+				MessageBox.Show("You must select existing svn.exe under step 0.");
+			}
+			else if (this.tbNewSvnRootUrl.Text.IsNullOrWhiteSpace())
+			{
+				MessageBox.Show("You must specify new svn root url under step 5.");
+			}
+			else
+			{
+
+				this._allItems.ForEach(item => item.svnExePath = this.tbSvnExePath.Text);
+				this._allItems.ForEach(item => item.newSvnRootUrl = this.tbNewSvnRootUrl.Text);
+			}
+
+
+			var log = new CustomTraceLog("Relocating to " + this.tbNewSvnRootUrl + " ...", true, false, (traceLog, line, newLine, level, ending, forNewLine) => { this.tbOutput.AppendText(line + "\r\n"); this.tbOutput.ScrollToCaret(); });
+			List<string> allAllowedProjects = this.tbAllProjects.Text.ToLower().ToLines(false, false, new List<string>());
+			List<string> svnRootUrlsFound = this.tbSvnRootUrlsFound.Text.ToLower().ToLines(false, false, new List<string>());
+
+			string batchContent = string.Empty;
+			foreach (var item in this._allItems)
+			{
+				item.Relocate(log, allAllowedProjects, svnRootUrlsFound, ref batchContent, this.cbExecuteImmediately.Checked);
+			}
+
+			if (this.cbWriteToBatFile.Checked)
+			{
+				string batchFilePath = BuildingBlocks.Common.Misc.ApplicationPhysicalExeFilePathWithoutExtension + ".bat";
+				using (log.LogScope("Writing to " + batchFilePath + "..."))
+				{
+					
+					if (File.Exists(batchFilePath))
+					{
+						using (log.LogScope("Deleting old file ..."))
+						{
+							File.Delete(batchFilePath);
+						}
+					}
+					File.WriteAllText(batchFilePath, batchContent);
+				}
+			}
+
+			if (log.ToString().Contains("[Failed.]"))
+			{
+				log.AddLine("DONE WITH ERROR(S).");
+			}
+			else
+			{
+				log.AddLine("DONE WITH SUCCESS.");
+			}
+			
+		}
+	}
+
+	public class Item
+	{
+		public string svnExePath;
+		public string folder;
+		public string svnUrl;
+
+		public string svnRootUrl
+		{
+			get
+			{
+				return BuildingBlocks.UI.Web.UriHandler.GetAuthority(this.svnUrl);
+			}
+		}
+
+		public string newSvnRootUrl;
+
+		public Item(string svnExePath, string svnFolder, string newSvnRootUrl)
+		{
+			this.svnExePath = svnExePath;
+			this.folder = Path.GetDirectoryName(svnFolder);
+			this.newSvnRootUrl = newSvnRootUrl;
+			var  log = new CustomTraceLog();
+			int exitCode = BuildingBlocks.UI.Console.ExecuteCommand(svnExePath, "info --show-item url", 3000, this.folder,log);
+			if (exitCode != 0)
+			{
+				MessageBox.Show(log.ToString());
+			}
+			else
+			{
+				// Files\TortoiseSVN\bin\svn.exe info --show-item url
+				//2016.12.29 21:58:10(local)   Command executing...
+				//2016.12.29 21:58:10(local) >> https://craftsynth.cloudapp.net:8443/svn/F4CIO
+				//2016.12.29 21:58:10(local)   ExitCode: 0}
+				this.svnUrl = log.ToString().Replace("\n", "|").GetSubstring(">> ", "\r");
+			}
+
+		}
+
+		public string svnNewUrl
+		{
+			get
+			{
+				if (this.svnExePath.IsNOTNullOrWhiteSpace() && this.svnRootUrl.IsNOTNullOrWhiteSpace() && this.folder.IsNOTNullOrWhiteSpace())
+				{
+					return  this.svnUrl.Replace(this.svnRootUrl, this.newSvnRootUrl.ToNonNullString());
+				}
+				else
+				{
+					return "";
+				}
+			}
+		}
+
+		public void Relocate(CustomTraceLog log, List<string> allAlowedProjects, List<string> allowedSvnRootUrlsToRelocate, ref string batchContent, bool executeImmediately)
+		{
+			using (log.LogScope("Processing "+this.folder+"..."))
+			{
+				if (!allAlowedProjects.Exists(line => line.StartsWith(this.folder.ToLower() + " | ")))
+				{
+					log.AddLine("project skipped.", false);
+				}
+				else if (!allowedSvnRootUrlsToRelocate.Exists(url => url == this.svnRootUrl))
+				{
+					log.AddLine("svn root url "+this.svnRootUrl+" skipped.", false);
+				}
+				else
+				{
+					using (log.LogScope(executeImmediately? "Relocating ":"Adding script for " + this.svnUrl + " ==> "+this.svnNewUrl+" ..."))
+					{
+						if (executeImmediately)
+						{
+							int exitCode = BuildingBlocks.UI.Console.ExecuteCommand(svnExePath, "relocate " + this.svnNewUrl, 10000, this.folder, log);
+							if (exitCode != 0)
+							{
+								log.AddLine("[Failed.]");
+							}
+						}
+
+						batchContent += "\r\nCD \"" + this.folder + "\"";
+						batchContent += "\r\n\"" + this.svnExePath + "\" relocate "+this.svnNewUrl;
+						batchContent += "\r\n";
+					}
+				}
+			}
+		}
+	}
+
+}
